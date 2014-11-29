43156d8297771a22e90d2ec154f2e448f340b8a6
Steven Kauyedauty <skauyedauty@gmail.com>
2014-11-28 23:44:49 -0700

    submit and clear ffunctions

--- main.js
+++ main.js
@@ -5,31 +5,39 @@ var app = angular.module('myApp',[]).
 			// $scope.tipPercent =0.05; 
 			//  possibly could do 
 
+
 			var defaultForm={
 				mealPrice: "",
 				mealTax: "",
 				tipPercent:""
 			}
+			// variables for my earnings Info;
+			$scope.mealCount = 0;
+			$scope.tipTotal = 0;
+			$scope.averageTip = 0;
 
 			$scope.user = angular.copy(defaultForm);
 
-			$scope.subtotal=function(a,b) {
-				var a = $scope.mealPrice;
-				var b = $scope.mealTax;
+
+
+			$scope.subtotal=function() {
+				var a = $scope.user.mealPrice;
+				var b = $scope.user.mealTax;
 				// var a = mealPrice;
 				// var b = mealTax;
+				console.log(a,b);
 				return a + (a *b);
 			}
 
-			$scope.tip= function(a,b) {
+			$scope.tip= function() {
 				var a = $scope.subtotal();
-				var b = $scope.tipPercent;
+				var b = $scope.user.tipPercent;
 				// var a = $scope.subtotal();
 				// var b = tipPercent;
 				return a * b;
 			}
 
-			$scope.total = function(a,b) {
+			$scope.total = function() {
 				var a = $scope.tip();
 				var b = $scope.subtotal();
 				// var a = $scope.tip();
@@ -38,33 +46,42 @@ var app = angular.module('myApp',[]).
 			}
 
 			$scope.submit = function() {
-				console.log($scope.myForm);
-				$scope.ready = $scope.myForm.$valid;
+				console.log("hello");
+				$scope.calculateEarnings();
+				$scope.user.mealPrice= null;
+				$scope.user.mealTax=null;
+				$scope.user.tipPercent =null;
+
+
 				// $scope.myForm = angular.copy(myForm);
 			}
 
-			// ***********cannot figure out why this will not work!!!!!!!
+
+			
 
 			$scope.cancel = function() {
-				$scope.myForm.$setPristine();
-				$scope.user = angular.copy(defaultForm);
-				// $scope.mealPrice ="";
-				// $scope.mealTax = 0.05;
-				// $scope.tipPercent = 0.05;
-				console.log('empty');
+				$scope.user.mealPrice= null;
+				$scope.user.mealTax=null;
+				$scope.user.tipPercent =null;
 			}
 
 			$scope.calculateEarnings = function(){
+				$scope.mealCount +=1;
+				$scope.tipTotal +=$scope.tip();
+				$scope.averageTip = $scope.tipTotal/$scope.mealCount;
+			}
+
+			$scope.reset= function() {
+				$scope.myForm.$setPristine();
+				$scope.user = angular.copy(defaultForm);
+				$scope.mealCount = 0;
+				$scope.tipTotal=0;
+				$scope.averageTip =0;
 
 			}
 
 
-			// works!!!!!!!!!!!!!!!
-			// $scope.cancel = function() {
-			// 	$scope.mealPrice ="" ;
-			// 	$scope.mealTax = 0.05;
-			// 	$scope.tipPercent =0.05; 
-			// }
+			
 			
 
 
